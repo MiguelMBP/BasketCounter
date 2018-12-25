57547ea85f3d23880a90240c356fbf22fbab1b79
MiguelMBP <miguelmbp98@gmail.com>
2018-12-25 12:04:01 +0100

    Mejoras en el Navigation Drawer

--- app/src/main/java/com/example/android/basketcounter/activities/MainActivity.java
+++ app/src/main/java/com/example/android/basketcounter/activities/MainActivity.java
@@ -1,95 +1,111 @@
 package com.example.android.basketcounter.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.basketcounter.R;
 import com.example.android.basketcounter.fragments.CounterFragment;
 import com.example.android.basketcounter.fragments.MatchListFragment;
 import com.example.android.basketcounter.fragments.TeamListFragment;
 
-public class MainActivity extends AppCompatActivity {
-
-    private String[] mNavOptions;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        mNavOptions = getResources().getStringArray(R.array.navOptions);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mNavOptions));
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        //Counter Fragment selected on startup
+        navigationView.getMenu().getItem(0).setChecked(true);
 
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, new CounterFragment())
+                .commit();
     }
 
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
-    /**
-     * Swaps fragments in the main content view
-     */
-    private void selectItem(int position) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
         Fragment fragment = null;
-        switch (position) {
-            case 0:
-                fragment = new CounterFragment();
-                break;
-            case 1:
-                fragment = new TeamListFragment();
-                break;
-            case 2:
-                fragment = new MatchListFragment();
-                break;
-            case 3:
-                fragment = new TeamListFragment();
-                break;
-        }
-        // Create a new fragment and specify the planet to show based on position
-        /*Fragment fragment = new PlanetFragment();
-        Bundle args = new Bundle();
-        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
-        fragment.setArguments(args);*/
 
-        // Insert the fragment by replacing any existing fragment
+        int id = item.getItemId();
+
+        if (id == R.id.nav_counter) {
+            fragment = new CounterFragment();
+
+        } else if (id == R.id.nav_Teams) {
+            fragment = new TeamListFragment();
+
+        } else if (id == R.id.nav_Matches) {
+            fragment = new MatchListFragment();
+
+        } else if (id == R.id.nav_NBA) {
+            fragment = new MatchListFragment();
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, fragment)
                 .commit();
 
-        // Highlight the selected item, update the title, and close the drawer
-        mDrawerList.setItemChecked(position, true);
-        //setTitle(mNavOptions[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-    }
-
 }
