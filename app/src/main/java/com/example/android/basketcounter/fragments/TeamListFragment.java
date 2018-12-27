7010bb45ad941251b0b06f66eeefe8e3ae86f7e3
MiguelMBP <miguelmbp98@gmail.com>
2018-12-27 11:47:40 +0100

    Room funcional en los equipos con ViewModel

--- app/src/main/java/com/example/android/basketcounter/fragments/TeamListFragment.java
+++ app/src/main/java/com/example/android/basketcounter/fragments/TeamListFragment.java
@@ -4,12 +4,15 @@ package com.example.android.basketcounter.fragments;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.android.basketcounter.R;
 import com.example.android.basketcounter.adapters.TeamAdapter;
@@ -24,12 +27,15 @@ import java.util.Observer;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TeamListFragment extends Fragment {
+    private AlertDialog.Builder builder;
+    private AlertDialog dialog;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private TeamAdapter adapter;
@@ -70,13 +76,13 @@ public class TeamListFragment extends Fragment {
 
         recyclerView.setAdapter(adapter);
 
-        /*model = ViewModelProviders.of(this).get(TeamViewModel.class);
+        model = ViewModelProviders.of(this).get(TeamViewModel.class);
         model.getTeams().observe(this, new androidx.lifecycle.Observer<List<Team>>() {
             @Override
             public void onChanged(@Nullable List<Team> teams) {
                 adapter.addTeams(teams);
             }
-        });*/
+        });
 
         return view;
     }
@@ -91,11 +97,46 @@ public class TeamListFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.add_team:
-                Team t = new Team("Equipo0");
+                createPopUp();
+
 
                 return true;
         }
         return false;
+    }
+
+    private void createPopUp() {
+
+        builder = new AlertDialog.Builder(getActivity());
+        View view= LayoutInflater.from(getActivity()).inflate(R.layout.popup_team, null);
+
+        builder.setView(view);
+
+        dialog= builder.create();
+        dialog.show();
+
+        final EditText teamName= view.findViewById(R.id.popupTeamName);
+        Button saveButton= view.findViewById(R.id.popupSaveProductButton);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!TextUtils.isEmpty(teamName.getText())) {
+
+                    addTeam(teamName.getText().toString());
+
+                }
+
+            }
+        });
+    }
+
+    private void addTeam(String teamName) {
+        Team team = new Team(teamName);
+        model.addTeam(team);
+
+        dialog.dismiss();
     }
 
     @Override
---
+++ app/src/main/java/com/example/android/basketcounter/viewmodel/TeamViewModel.java
@@ -0,0 +1,65 @@
+package com.example.android.basketcounter.viewmodel;
+
+import android.app.Application;
+
+import android.os.AsyncTask;
+
+import android.widget.Toast;
+
+import com.example.android.basketcounter.data.DataBaseRoom;
+import com.example.android.basketcounter.model.Team;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class TeamViewModel extends AndroidViewModel {
+
+    private LiveData<List<Team>> teamsList;
+    private static DataBaseRoom db;
+
+    public TeamViewModel(@NonNull Application application) {
+        super(application);
+        db = DataBaseRoom.getInstance(application);
+        this.teamsList = db.teamDAO().getAll();
+    }
+
+    public LiveData<List<Team>> getTeams() {
+        return teamsList;
+    }
+
+    public void addTeam(Team team) {
+    new AsyncAddTeamDB().execute(team);
+    }
+
+    private class AsyncAddTeamDB extends AsyncTask<Team, Void, Long> {
+
+        Team team;
+
+        @Override
+        protected Long doInBackground(Team... teams) {
+            long id = -1;
+            if (teams.length != 0) {
+                team = teams[0];
+                id = db.teamDAO().insertTeam(teams[0]);
+                team.setTid(id);
+            }
+
+            return id;
+        }
+
+        @Override
+        protected void onPostExecute(Long id) {
+            if (id == -1) {
+                Toast.makeText(getApplication(), "Error adding team", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                Toast.makeText(getApplication(), "Team added", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+        }
+    }
+}
